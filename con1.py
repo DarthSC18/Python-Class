53ed63a90c5352e3b36c699f506b8a2abce557ad
DarthSC18 <shivam18001@gmail.com>
2025-02-12 11:48:21 +0530

    Create con1.py

---
+++ con1.py
@@ -0,0 +1,10 @@
+class student:
+    def __init__(self):
+        print("Constructor is called")
+
+    def display(self):
+        print("inside Display method")
+
+s1=student() #s1 object created
+s1.display()
+
