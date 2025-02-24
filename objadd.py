de8d9168ddc0a4ecab12a2bef88c30ebbee51b04
DarthSC18 <shivam18001@gmail.com>
2025-02-24 09:09:04 +0530

    Create objadd.py
    Practical 7 on 24/02/2025

---
+++ objadd.py
@@ -0,0 +1,23 @@
+#write a python code to perform addition of 2 com plex numbers by passing object as a parameter
+#write a python code to perform addition of 2 complex numbers by passing object as a parameter
+#self.variable=current objec.variable
+#prac7:24-02025
+class complex:
+    def __init__(self,real,img):
+        self.real=real
+        self.img=img
+
+    def add(self,other):
+        real=self.real+other.real
+        img=self.img+other.img
+        return complex(real,img)
+
+    def display(self):
+        print(f"{self.real}+{self.img}I")
+
+c1=complex(2,3)
+c2=complex(4,5)
+c3=c1.add(c2)
+c1.display()
+c2.display()
+c3.display()
