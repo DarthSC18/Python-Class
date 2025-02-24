0f7183994d0a462ded4d77761dfbb23e402057d0
DarthSC18 <shivam18001@gmail.com>
2025-02-24 10:36:42 +0530

    Create greatinher.py

---
+++ greatinher.py
@@ -0,0 +1,21 @@
+#write a python code to calc greatest of 2 numbers using inheritsnce
+#prac 7-4
+class Number1:
+    def __init__(self,x):
+        self.x=x
+class Number2(Number1):
+    def __init__(self,x,y):
+        super().__init__(x)
+        self.y=y
+    def great(self):
+        if self.x==self.y:
+            print("Numbers are Equal")
+        elif self.x==self.y:
+            print(f"{self.x} is greater than {self.y}")
+        else:
+            print(f"{self.y} is greater than {self.x}")
+
+x=int(input("Enter the First Number: "))
+y=int(input("Enter the Second Number: "))
+z=Number2(x,y)
+z.great()
